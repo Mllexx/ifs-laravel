5465c716e9d779b4d1d3a10536b634551a840b2d
Andrew Muleke <mulekea3@gmail.com>
2026-01-02 13:08:58 +0300

    Update Customer Service Sync Service

--- src/Services/CustomerService.php
+++ src/Services/CustomerService.php
@@ -8,6 +8,7 @@ use Mllexx\IFS\Http\IFSClient;
 use App\Models\IFSCustomers;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class CustomerService
 {
@@ -17,6 +18,8 @@ class CustomerService
      * @var IFSClient
      */
     protected IFSClient $client;
+
+
 
     /**
      * The base endpoint for customer operations
@@ -252,36 +255,45 @@ class CustomerService
      * @param int $companyId
      * @return array
      */
-    public function syncCustomers($companyId=1){
+    public function syncCustomers($companyId=1,$overwrite=true){
         Log::info("Syncing customers from IFS");
         //TODO: Implement batching if there are many customers
         //TODO: Implement last synced at to only get new/updated customers
         //TODO: Implement logic to check existence of sync tables if not exists create them (run migrations if not exists)
         try{
-            $customers = $this->list();
+            DB::beginTransaction();
+            if($overwrite){
+                Log::info("Clearing existing customers from database");
+                IFSCustomers::where('company_id',$companyId)->delete();
+            }
+            //$customers = $this->list();
+            $customers = $this->listAggregated();
             Log::info("Adding customers to database");
             $lastSyncedAt = Carbon::now();
             foreach ($customers as $customer) {
                 IFSCustomers::updateOrCreate([
                     'company_id' => $companyId,
-                    'customer_id' => $customer->CustomerId,
-                    'name' => $customer->Name,
-                    'party' => $customer->Party,
-                    'country' => $customer->Country,
-                    'b2b_customer' => $customer->B2bCustomer,
-                    'corporate_form' => $customer->CorporateForm,
-                    'creation_date' => $customer->CreationDate, 
-                    'currency_code' => $customer->CurrencyCode, 
+                    'customer_id' => $customer->C2_CUSTOMER_ID,
+                    'name' => $customer->C3_CUSTOMER_NAME,
+                    'party' => $customer->C4_PARTY_TYPE,
+                    'country' => $customer->C8_COUNTRY,
+                    //'b2b_customer' => $customer->B2bCustomer,
+                    //'corporate_form' => $customer->CorporateForm,
+                    'creation_date' => $customer->C7_CREATION_DATE, 
+                    'currency_code' => $customer->C5_CURRENCY, 
                     'last_synced_at' => $lastSyncedAt,
                 ]);
             }
+            DB::commit();
             Log::info("Customers added to database");
             return [
                 'success' => true,
                 'message' => "IFS Customers synced successfully.",
             ];
         }catch(\Exception $e){
+            DB::rollBack();
             Log::error("Failed to sync customers. Error: ". $e->getMessage());
+            report($e);
             return [
                 'success' => false,
                 'message' => 'Failed to sync IFS Customers. Error: '. $e->getMessage(),
